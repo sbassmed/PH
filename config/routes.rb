1ce17b1a724df97055de8b38e89de2f017bacd55
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-25 00:25:42 -0500

    Crea la ruta post/products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   get 'products', to: 'products#index'
-  get '/products/new', to: 'products#new'
+  get '/products/new', to: 'products#new', as: 'new_product'
+  #ruta formulario producto
+  post "products", to: 'products#create'
+
+  #ruta para crear el producto
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
