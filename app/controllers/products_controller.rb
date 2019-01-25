7f966b72de9bbcdf004473547ccc5c6e177a012e
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-25 01:14:17 -0500

    Crea la ruta /products/id/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+  def edit
+  @product = Product.find(params[:id])
+  end
 
   private
     def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   #ruta formulario producto
   post "products", to: 'products#create'
   #ruta para crear el producto
-  get '/products/:id', to: 'products#show'
+  get '/products/:id', to: 'products#show', as: 'product'
+  #muestra el prodcuto del id seleccionado
+  get '/products/:id/edit', to: 'products#edit'
+  #ruta para editar producto
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
